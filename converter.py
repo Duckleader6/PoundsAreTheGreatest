fed5047ec63630790b10f61d866da6ad0907488c
Duckleader6 <duckleader6@gmail.com>
2026-05-09 05:07:38 +0000

    continued

--- converter.py
+++ converter.py
@@ -1,15 +1,157 @@
 import math
-from_unit = int(input("What would you like to convert from?\n(1)Pounds\n(2)Something Else\n"))
-if from_unit == 1:
-    number1 = float(input("How many pounds?\n"))
-    option1 = int(input("What Group of Stuff?\n(1)Convential Units\n"))
-    if option1 == 1:
-        to_unit = int(input("What would you like to convert to?\n(1)Grams\n(2)Milligrams\n(3)Kilograms\n(4)Metric Ton\n(5)Ounce\n(6)Pouns\n(7)Stone\n(8)Hundredweight\n(9)US Ton\n(10)Imperial Ton\n"))
-        if to_unit == 1:
-            
-elif from_unit == 2:
-    print("Under Construction")
-else:
-    print("Brother Just put in a real answer.")
+
+while True:
+    from_unit = int(input("What would you like to convert from?\n(1)Pounds\n(2)Something Else BTW if it is something else you can only go to pounds.\n"))
+    if from_unit == 1:
+        number1 = float(input("How many pounds?\n"))
+        option1 = int(input("What Group of Stuff?\n(1)Convential Units\n(2)Unconventional Units (still real just not very usable)\n(3)Just stupid stuff\n"))
+        if option1 == 1:
+            to_unit = int(input("What would you like to convert to?\n(1)Grams\n(2)Milligrams\n(3)Kilograms\n(4)Metric Ton\n(5)Ounce\n(6)Pound\n(7)Stone\n(8)Hundredweight\n(9)US Ton\n"))
+            if to_unit == 1:
+                print(number1 * 453.592)
+            elif to_unit == 2:
+                print(number1 * 453592)
+            elif to_unit == 3:
+                print(number1 * 0.453592)
+            elif to_unit == 4:
+                print(number1 * 0.000453592)
+            elif to_unit == 5:
+                print(number1 * 16)
+            elif to_unit == 6:
+                print(number1)
+            elif to_unit == 7:
+                print(number1 * 0.0714286)
+            elif to_unit == 8:
+                print(number1 * 0.02)
+            elif to_unit == 9:
+                print(number1 * 0.0005)
+        if option1 == 2:
+            option2 = int(input("What group of stuff?\n(1)Troy and Apothecary units\n(2)Tower Weights\n(3)Roman Units\n(4)Babylonian Units\n(5)Ancient Egyptian Units\n(6)Misc stuff that I might organize later\n"))
+            if option2 == 1:
+                to_unit = int(input("What would you like to convert to?\n(1)Grains\n(2)Pennyweights\n(3)Troy Ounces\n(4)Troy Pounds\n(5)Apothecary Ounces\n(6)Apothecary Pounds\n(7)Dram\n(8)Scruple\n"))
+                if to_unit == 1:
+                    print(number1 * 7000)
+                elif to_unit == 2:
+                    print(number1 * 700)
+                elif to_unit == 3:
+                    print(number1 * 14.5833)
+                elif to_unit == 4:
+                    print(number1 * 0.583333)
+                elif to_unit == 5:
+                    print(number1 * 12)
+                elif to_unit == 6:
+                    print(number1 * 0.75)
+                elif to_unit == 7:
+                    print(number1 * 256)
+                elif to_unit == 8:
+                    print(number1 * 480)
+            if option2 == 2:
+                to_unit = int(input("What would you like to convert to?\n(1)Tower Ounces\n(2)Tower Pounds\n"))
+                if to_unit == 1:
+                    print(number1 * 7000 / 450)
+                if to_unit == 2:
+                    print(number1 * 7000 / 450 / 12)
+            if option2 == 3:
+                to_unit = int(input("What would you like to convert to?\n(1)Uncia\n(2)Libra\n(3)Siliqua\n(4)Deunx\n(5)Dextans\n(6)Dodrans\n(7)Bes\n(8)Septunx\n(9)Semis\n(10)Quincunx\n(11)Triens\n(12)Quadrans\n(13)Sextans\n(14)Scrupulum\n(15)Obulos\n(16)Sicilicus"))
+                if to_unit == 1:
+                    print(number1 * 1.404 * 12)
+                if to_unit == 2:
+                    print(number1 * 1.404)
+                if to_unit == 3:
+                    print(number1 * 1.404 * 12 * 144)
+                if to_unit == 4:
+                    print(number1 * 1.404 * 12 * 11)
+                if to_unit == 5:
+                    print(number1 * 1.404 * 12 * 11)
+                if to_unit == 6:
+                    print(number1 * 1.404 * 12 * 11)
+                if to_unit == 7:
+                    print(number1 * 1.404 * 12 * 11)
+                if to_unit == 8:
+                    print(number1 * 1.404 * 12 * 11)
+                if to_unit == 9:
+                    print(number1 * 1.404 * 12 * 11)
+                if to_unit == 10:
+                    print(number1 * 1.404 * 12 * 11)
+                if to_unit == 11:
+                    print(number1 * 1.404 * 12 * 11)
+                if to_unit == 12:
+                    print(number1 * 1.404 * 12 * 11)
+                if to_unit == 13:
+                    print(number1 * 1.404 * 12 * 11)
+                if to_unit == 14:
+                    print(number1 * 1.404 * 12 * 24)
+                if to_unit == 15:
+                    print(number1 * 1.404 * 12 * 48)
+                if to_unit == 16:
+                    print(number1 * 1.404 * 12 * 24)
+            if option2 == 4:
+                to_unit = int(input("What would you like to convert to?\n(1)Shekel\n(2)Mina\n(3)Talent\n"))
+                if to_unit == 1:
+                    print(number1 * 453.592 *11.25)
+                if to_unit == 2:
+                    print(number1 * 453.592 *11.25 * 60)
+                if to_unit == 3:
+                    print(number1 * 453.592 *11.25* 3600)
+            if option2 == 5:
+                to_unit = int(input("What would you like to convert to?\n(1)Deben\n(2)Kite\n(3)Shematy\n"))
+                if to_unit == 1:
+                    print(number1 / 0.198)
+                if to_unit == 2:
+                    print(number1 / 0.198 / 10)
+                if to_unit == 3:
+                    print(number1 / 0.198 / 12)
+        if option1 == 3:
+            option2 = int(input("What group of stuff?\n(1)Cosmic Stuff\n(2)Currencies"))
+            if option2 == 1:
+                option3 = int(input("What group of stuff?\n(1)Milky Way\n"))
+                if option3 == 1:
+                    to_unit = int(input("What would you like to convert it to?\n(1)Earth\n(2)Moon\n(3)Mercury\n(4)Mars\n(5)Jupiter\n(6)Saturn\n(7)Uranus\n(8)Neptune\n(9)Pluto\n"))
+                    if to_unit == 1:
+                        print(number1 / 1.37e25)
+                    if to_unit == 2:
+                        print(number1 / 1.62e23)
+                    if to_unit == 3:
+                        print(number1 / 7.28e23)
+                    if to_unit == 4:
+                        print(number1 / 1.41e24)
+                    if to_unit == 5:
+                        print(number1 / 4.184e27)
+                    if to_unit == 6:
+                        print(number1 / 1.25e27)
+                    if to_unit == 7:
+                        print(number1 / 1.91e26)
+                    if to_unit == 8:
+                        print(number1 / 2.25e26)
+                    if to_unit == 9:
+                        print(number1 / 2.87e22)
+    if from_unit == 2:
+        number1 = float(input("How many of that unit?\n"))
+        option1 = int(input("What Group of Stuff?\n(1)Convential Units\n(2)Unconventional Units (still real just not very usable)\n"))
+        if option1 == 1:
+            to_unit = int(input("What would you like to convert to?\n(1)Grams\n(2)Milligrams\n(3)Kilograms\n(4)Metric Ton\n(5)Ounce\n(6)Pound\n(7)Stone\n(8)Hundredweight\n(9)US Ton\n"))
+            if to_unit == 1:
+                print(number1/453.592)
+            elif to_unit == 2:
+                print(number1/453592)
+            elif to_unit == 3:
+                print(number1/0.453592)
+            elif to_unit == 4:
+                print(number1/0.000453592)
+            elif to_unit == 5:
+                print(number1/16)
+            elif to_unit == 6:
+                print(number1)
+            elif to_unit == 7:
+                print(number1/0.0714286)
+            elif to_unit == 8:
+                print(number1/0.02)
+            elif to_unit == 9:
+                print(number1/0.0005)
+
+    if from_unit < 1 or from_unit > 2:
+        print("Brother put in a real input")
+    if input("Do you want to convert something else? (y/n)\n") == "n":
+        break
 #to_unit = input("What would you like to convert to?")
 #value = float(input("What is the value you would like to convert?"))
