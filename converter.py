6f5196be3172d8513a0dded4c11f6761f31acab9
Duckleader6 <duckleader6@gmail.com>
2026-05-10 05:51:59 +0000

    fisxed misc

--- converter.py
+++ converter.py
@@ -190,7 +190,7 @@ async def main():
 
 #pounds to misc
                 if option2 == 4:
-                    to_unit = int(await input("What would you like to convert it to?\n(1)Party sized bag of hot cheetos\n(2)Large Blahaj\n(3)Small Blahaj\n(4)Hollow Knight for the Nintendo Switch)\n(5)Standard Box of Kraft Mac and Cheese\n(6)Large Box of Kraft Mac and Cheese\n"))
+                    to_unit = int(await input("What would you like to convert it to?\n(1)Party sized bag of hot cheetos\n(2)Large Blahaj\n(3)Small Blahaj\n(4)Hollow Knight for the Nintendo Switch\n(5)Standard Box of Kraft Mac and Cheese\n(6)Large Box of Kraft Mac and Cheese\n"))
                     if to_unit == 1:
                         print(number1 / 1.12)
                     if to_unit == 2:
